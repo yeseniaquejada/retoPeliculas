7c1701568c7ba1f2e751704e3eaac7a86d227f59
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2021-08-11 15:20:05 -0500

    Yesenia

---
+++ data/data.js
@@ -0,0 +1,34 @@
+export const data = 
+[
+    {
+        "id": 1,
+        "name": "Black Widow",
+        "image": "/images/1.jpg",
+    },
+    {
+        "id": 2,
+        "name": "Spider-Man: Far From Home",
+        "image": "/images/2.png",
+    },
+    {
+        "id": 3,
+        "name": "Avengers: Endgame",
+        "image": "/images/y.jpg",
+    },
+    {
+        "id": 4,
+        "name": "Capitana Marvel",
+        "image": "/images/4.png",
+    },
+    {
+        "id": 5,
+        "name": "Thor: Ragnarok",
+        "image": "/images/5.png",
+    }
+    ,
+    {
+        "id": 6,
+        "name": "Pantera Negra (película)",
+        "image": "/images/6.1.jpg",
+    }
+]
