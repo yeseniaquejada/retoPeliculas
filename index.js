d64d758d4701a500bfb249e19b2fdd48f41580d7
LuisCorralesM <luiscorrales5012@gmail.com>
2021-08-11 17:48:24 -0500

    Luis
    idenatacion

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import { datos } from './data/data.js';
 const items = document.getElementById('items');
 const templateCard = document.getElementById('template-card').content;
 const fragment = document.createDocumentFragment();
-let like = [], disLike = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     loadData(datos);
@@ -11,8 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 const loadData = datos => {
-    datos.forEach(personaje => {
-        const { id, name, image } = personaje;
+    datos.forEach(pelicula => {
+        const { id, name, image } = pelicula;
         templateCard.querySelector('h5').textContent = name;
         templateCard.querySelector('img').setAttribute('src', image);
         const clone = templateCard.cloneNode(true);
@@ -22,42 +21,18 @@ const loadData = datos => {
     items.appendChild(fragment);
 }
 
-items.addEventListener('click', e => {
-    addLike(e);
+document.addEventListener('click', e => {
+    if(e.target.matches('#imagenClickeada')){
+        console.log(e.target);  
+        items.remove()
+        
+        // templateCard.querySelector('h5').textContent = name;
+        // templateCard.querySelector('img').setAttribute('src', image);
+        // const clone = templateCard.cloneNode(true);
+        // fragment.appendChild(clone);
+        
+    }
 })
 
-const addLike = e => {
-    //que contenga btn dark y devuelve true
-    if (e.target.classList.contains('btn-dark')) {
-        //captura todos los elementos de la target
-        setLike(e.target.parentElement);
-    }
-}
 
-const setLike = object => {
-    const label = object.querySelector('#like')
-    let cantidad = Number(label.textContent) || 0
-    cantidad++
-    label.textContent = cantidad
-  }
 
-//disLike   
-
-items.addEventListener('click', a => {
-    adddisLike(a);
-})
-
-const adddisLike = a => {
-    //que contenga btn dark y devuelve true
-    if (a.target.classList.contains('btn-white')) {
-        //captura todos los elementos de la target
-        setdisLike(a.target.parentElement);
-    }
-}
-
-const setdisLike = object => {
-    const label = object.querySelector('#disLike')
-    let cantidad = Number(label.textContent) || 0
-    cantidad++
-    label.textContent = cantidad
-  }
