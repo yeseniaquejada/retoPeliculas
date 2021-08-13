049af7feda712f14d7d6a9e00dc8e161e3f11a15
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2021-08-12 23:13:01 -0500

    Yesenia
    formulario

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ import { datos } from './data/data.js';
 const items = document.getElementById('items');
 const item = document.getElementById('item');
 const contenedorFormulario = document.querySelector('#formulario')
+let arreglo = [];
 const templateCard = document.getElementById('template-card').content;
 const templateCard2 = document.getElementById('template-card2').content;
 const templateCard3 = document.getElementById('template-card3').content;
+
 const fragment = document.createDocumentFragment();
 document.addEventListener('DOMContentLoaded', () => {
     loadData(datos);
 })
 
 
+// codigo tarjetas
 const loadData = datos => {
     datos.forEach(pelicula => {
         const { id, name, image } = pelicula;
@@ -25,6 +28,7 @@ const loadData = datos => {
     items.appendChild(fragment);
 }
 
+// codigo hacer click en la tarjeta
 document.addEventListener('click', e => {
     if(e.target.matches('#imagenClickeada')){
         console.log(e.target); 
@@ -53,6 +57,34 @@ document.addEventListener('click', e => {
         // item.remove()
         console.log(templateCard3);
         contenedorFormulario.appendChild(templateCard3)
+        const formularioP = document.getElementById('formularioP');
+        formularioP.addEventListener('submit', (evento) => {
+            evento.preventDefault();
+            let nom = document.getElementById('nom').value;
+            let apellido = document.getElementById('apellido').value;
+            let telefono = document.getElementById('telefono').value;
+            let direccion = document.getElementById('direccion').value;
+            let observaciones = document.getElementById('observaciones').value;
+            console.log(nom, apellido, telefono, direccion, observaciones);
+            crearRegistro(nom, apellido, telefono, direccion, observaciones);
+            guardarDatos();
+        })
     }
 
 })
+
+const crearRegistro = (nombre,apellido,telefono,direccion, observaciones) => {
+    let registro = {
+        nombre: nombre,
+        apellido: apellido,
+        telefono: telefono,
+        direccion: direccion,
+        observaciones: observaciones
+    }
+    arreglo.push(registro);
+    console.log(arreglo);
+}
+
+const guardarDatos = () => {
+    localStorage.setItem('local', JSON.stringify(arreglo))
+}
